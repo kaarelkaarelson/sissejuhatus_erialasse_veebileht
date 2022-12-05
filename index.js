b15614148275805a7322c310c69520618d1d907f
Kaarel-Richard Kaarelson <kaarel-richard.kaarelson@ut.ee>
2022-12-05 21:32:41 +0200

    Lisan ruudi lehe. Muutsin pealehel koerte paigutust.

---
+++ index.js
@@ -0,0 +1,8 @@
+const nupp = document.getElementsByClassName("nimed");
+
+for (let i; i < nupp.length; i++) {
+  console.log(nupp[i]);
+}
+
+nupp.style = "background-color: red";
+console.log("Tere!");
